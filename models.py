339b78612e4998714f0e8979f351533e7d97c221
Luc Le Pottier <luclepot@lbl.gov>
2021-03-15 20:07:17 -0700

    add boilerplate code

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ import sklearn.base
 from abc import abstractmethod, ABC
 from sklearn.utils import check_array, check_random_state
 
-class atlas_anomaly_detection_base(sklearn.base.BaseEstimator, ABC):
+class anomaly_detection_base(sklearn.base.BaseEstimator, ABC):
     """base class which inherits from the sklearn base estimator. Provides broad functionality for 
     declaring new model classes, including defining save and load functions.
     
@@ -39,31 +39,95 @@ class atlas_anomaly_detection_base(sklearn.base.BaseEstimator, ABC):
     def predict(self, x):
         raise NotImplementedError
 
+    def _inputs_to_attributes(self, local_variables):
+        for k,v in local_variables.items():
+            if k != 'self':
+                setattr(self, k, v)
+        
 
-class SALAD(atlas_anomaly_detection_base):
+def _check_array_type(x):
+    if isinstance(x, pd.DataFrame):
+        return x.values
+    elif isinstance(x, np.ndarray):
+        return x
+    raise AttributeError('input array is of type "{}"; should be array'.format(type(x)))
+
+def _check_training_params(model, x, y, w=None):
+    """
+    checks training dataset parameters x, y, and w against model <model>, including shapes and types
+    """
+    x,y = _check_array_type(x), _check_array_type(y)
+    
+    if not isinstance(model, keras.Model):
+        raise AttributeError('model is not a keras.Model instance!')
+    
+    x_shape, y_shape = x.shape,y.shape
+    
+    input_match = model.input_shape[1] == np.array(x.shape)
+    output_match = model.output_shape[1] == np.array(y.shape)
+    
+    if not input_match.any():
+        raise AttributeError('x array shape {} does not match model input shape {}'.format(x_shape, model.input_shape))
+    if not output_match.any():
+        if len(output_match) == 1:
+            output_match = np.array([True])
+        else:
+            raise AttributeError('y array shape {} does not match model output shape {}'.format(y_shape, model.output_shape))
+
+    if len(input_match) > 2:
+        raise AttributeError('input array must have less than 3 dimensions')
+    if len(output_match) > 2:
+        raise AttributeError('output array must have less than 3 dimensions')
+    
+    if np.where(input_match)[0][0] == 0:
+        x = x.T
+    if np.where(output_match)[0][0] == 0:
+        y = y.T
+        
+    if w is not None:
+        w = _check_array_type(w)
+        if w.shape != y.shape:
+            if w.T.shape != y.shape:
+                raise AttributeError('weight array shape {} must match y array shape {}'.format(y.shape, w.shape))
+            else:
+                w = w.T
+    return x, y, w
+
+class SALAD(anomaly_detection_base):
     def __init__(
-        self, random_state=None, path=None,
-        sb_model=None, sr_model=None, optimizer='adam',
-        metrics=[], loss='binary_crossentropy'
-
+        self, path=None, sb_model=None, sr_model=None, 
+        optimizer='adam', metrics=[], loss='binary_crossentropy', 
+        sr_epochs=10, sb_epochs=10, compile=True, callbacks=[], 
+        sb_arch=None, sr_arch=None, test_size=0.3,
     ):
-        self.random_state = random_state
-        self.path = path
-        self.optimizer=optimizer
-        self.metrics=metrics
-        self.loss=loss
+        self._inputs_to_attributes(locals())
 
-        self.sb_model=sb_model
-        self.sr_model=sr_model
-        self.sb_arch=None
-        self.sr_arch=None
+    def fit_sb(self, x, y, w=None):
+        if self.sb_model is None:
+            raise ValueError('parameter <sb_model> is None. Please set it to a valid keras model.')
         
-    def fit_sb(self, x, y, epochs=10, compile=True):
-        self.random_state_sb = check_random_state(self.random_state)
-        
-    def fit_sr(self, x, y, epochs=10):
-        self.random_state_sr = check_random_state(self.random_state)
-
-    def fit(self, x_sb, y_sb, x_sr, y_sr, epochs_sb=10, epochs_sr=10, compile=True):
-        self.fig_sb(x_sb, y_sb, epochs_sb, compile=compile)
+        x, y, w = _check_training_params(self.sb_model, x, y, w)
+        self.sb_model.fit(
+            x, y,
+        )
         return 0
+
+    def fit_sr(self, x, y, w=None):
+        if self.sr_model is None:
+            raise ValueError('parameter <sb_model> is None. Please set it to a valid keras model.')
+        
+        x, y, w = _check_training_params(self.sr_model, x, y, w)
+        self.sr_model.fit(
+            x, y,
+        )
+        return 0 
+
+    def fit(self, x_sb, y_sb, x_sr, y_sr):
+        
+        self.fit_sb(x_sb, y_sb)
+        self.fit_sr(x_sr, y_sr)
+
+        return 0
+
+    def predict(self, x):
+        raise NotImplementedError()
